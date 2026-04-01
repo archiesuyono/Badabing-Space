e9c2b127e450a35fc32bbb426825b077a864f0cf
Hariz Diza Santoso <hariz.hds@gmail.com>
2026-04-01 14:55:31 +0700

    Revisi routes Index

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,30 @@
-const express = require('express')
-const router = express.Router()
-const loginRouter = require('./login')
-const exploreRouter = require('./explore')
-const profilesRouter = require('./profiles')
+const router = require("express").Router()
+const loginRoutes = require("./login")
+const profileRoutes = require("./profiles")
+const exploreRoutes = require("./explore")
+const user = require("../models/user")
+const Controller = require("../controllers/controller")
 
-router.use('/login', loginRouter)
-router.use('/explore', exploreRouter)
-router.use('/profiles', profilesRouter)
-
-router.get('/', (req, res) => {
-    res.redirect('/login')
+//routes
+router.get("/", (req, res) => {
+     res.redirect("/start")
 })
+
+router.use("/start", loginRoutes)
+
+router.use((req, res, next) => {
+     if (!req.session.user) {
+          res.redirect("/start")
+     } else {
+          next()
+     }
+})
+
+router.use("/explore", exploreRoutes)
+
+router.use("/profile", profileRoutes)
+
+//logout
+router.get("/logout", Controller.getLogout)
 
 module.exports = router
